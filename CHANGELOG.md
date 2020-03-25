905b60ba2de449c9c66cdeca60529b78833e37c9
Axel Haustant <noirbizarre@gmail.com>
2020-03-26 00:14:55 +0100

    Update to version 0.1.1.dev for next development cycle

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 # Changelog
 
+## In development
+
+- Nothing yet
+
 ## 0.1.0 (2020-03-26)
 
 - Nothing yet
--- drafts/__about__.py
+++ drafts/__about__.py
@@ -1,2 +1,2 @@
-__version__ = '0.1.0'
+__version__ = '0.1.1.dev'
 __description__ = 'Add a browsable drafts listing to your Pelican website'
